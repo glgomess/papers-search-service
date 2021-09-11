67c8ab3be49cceae01d28edb7bdea76b4430af02
Guilherme Lacerda <Guilherme.Gomes1@ibm.com>
2021-09-11 18:15:08 -0300

    Initial user queries

---
+++ src/database/queries.ts
@@ -0,0 +1,17 @@
+export default class QueryBuilder {
+  static insertNewUser(
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+  ) {
+    return `INSERT INTO USERS(EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
+            VALUES ('${email}', '${firstName}', '${lastName}', '${password}')`;
+  }
+
+  static findUserByEmail(email: string) {
+    return `SELECT ID
+            FROM USERS USR
+            WHERE USR.EMAIL = '${email}'`;
+  }
+}
