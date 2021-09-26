c8dd009e11aa58cc6fbc4ab67f294720cbebae33
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-09-26 18:27:37 -0300

    Utilizing default stack

--- src/errors/GenericError.ts
+++ src/errors/GenericError.ts
@@ -5,25 +5,22 @@ export default class GenericError extends CustomError {
 
   originalMessage: string;
 
-  stack: string;
-
   payload?: Object;
 
   constructor(
     message:string,
     statusCode:number,
     originalMessage:string,
-    stack: string,
     payload?: Object,
   ) {
     super(message);
 
     this.statusCode = statusCode;
     this.originalMessage = originalMessage;
-    this.stack = stack;
     if (payload) {
       this.payload = payload;
     }
+
     Object.setPrototypeOf(this, GenericError.prototype);
   }
 }
--- src/errors/UserError.ts
+++ src/errors/UserError.ts
@@ -5,25 +5,22 @@ export default class UserError extends CustomError {
 
   originalMessage: string;
 
-  stack: string;
-
   payload?: Object;
 
   constructor(
     message:string,
     statusCode:number,
     originalMessage:string,
-    stack: string,
     payload?: Object,
   ) {
     super(message);
 
     this.statusCode = statusCode;
     this.originalMessage = originalMessage;
-    this.stack = stack;
     if (payload) {
       this.payload = payload;
     }
+
     Object.setPrototypeOf(this, UserError.prototype);
   }
 }
